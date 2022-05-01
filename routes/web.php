a0bdb14b6c9a9982578d026b95443b497fff0bc9
ryutaro06 <nyukon.ryu305@icloud.com>
2022-05-01 00:08:27 +0000

    Laravel12まで実装

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 
@@ -26,3 +26,7 @@ Route::group(['prefix' => 'admin'], function() {
   Route::get('profile/edit', 'Admin\ProfileController@edit');
     
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
