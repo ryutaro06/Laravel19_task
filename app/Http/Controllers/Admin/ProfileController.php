218dc587fc6cc0588c113d2df48574f3f4c3739f
ryutaro06 <nyukon.ryu305@icloud.com>
2022-05-02 13:30:57 +0000

    PHP/Laravel 14_応用課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下を追記することでProfile Modelが扱えるようになる
+use App\Profile;
+
 class ProfileController extends Controller
 {
   //
@@ -27,7 +30,24 @@ class ProfileController extends Controller
         // 以下を追記
     public function create(Request $request)
     {
-        // admin/profile/createにリダイレクトする
-        return redirect('admin/profile/create');
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+      
+      $profile = new Profile;
+      $form = $request->all();
+      
+
+      
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
+      // admin/news/createにリダイレクトする
+      return redirect('admin/profile/create');
     }  
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>MyNews</title>
+    <title>MyProfile</title>
   </head>
   <body>
     
@@ -13,7 +13,7 @@
     @extends('layouts.profile')
     
     {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-    @section('title', 'ニュース')
+    @section('title', 'プロフィール')
 
     {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
     @section('content')
@@ -32,29 +32,29 @@
                 <div class="form-group row">
                     <label class="col-md-2">氏名(name)</label>
                     <div class="col-md-10">
-                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">性別(gender)</label>
                     <div class="col-md-10">
-                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">趣味(hobby)</label>
                     <div class="col-md-10">
-                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-md-2">自己紹介欄(introduction)</label>
                     <div class="col-md-10">
-                        <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                     </div>
                 </div>
                 {{ csrf_field() }}
-                <input type="submit" class="btn btn-primary" value="更新">
+                <input type="submit" class="btn btn-primary" value="送信">
             </form>
           </div>
           </div>
