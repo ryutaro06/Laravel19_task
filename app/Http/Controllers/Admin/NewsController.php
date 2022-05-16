da99cb2ec61556417daeaaeb16322ef981275a0e
ryutaro06 <nyukon.ryu305@icloud.com>
2022-05-16 10:00:41 +0000

    Heroku_S3

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,6 +12,8 @@ use App\News;
 use App\History;
 use Carbon\Carbon;
 
+use Storage; //追加
+
 class NewsController extends Controller
 {
   //
@@ -31,8 +33,8 @@ class NewsController extends Controller
       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
       // formに画像があれば、保存する
       if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+          $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+          $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -87,8 +89,8 @@ class NewsController extends Controller
       if ($request->remove == 'true') {
           $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
-          $path = $request->file('image')->store('public/image');
-          $news_form['image_path'] = basename($path);
+          $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
+          $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news_form['image_path'] = $news->image_path;
       }
